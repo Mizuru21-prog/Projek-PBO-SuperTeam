524509cb16db83daea113b9e88bb3f342cb14846
Anas Naufal Al-kiram <72111231+Mizuru21-prog@users.noreply.github.com>
2021-12-05 11:21:20 +0700

    Add files via upload

---
+++ TugasProjectSuper/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import menu.ApotekMenu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApotekMenu tes = new ApotekMenu();
+        tes.setVisible(true);
+    }
+    
+}
